a259da790ceb4571d21b77a1e9ffe4274760fcee
Axel Munch <92171197+axelmunch@users.noreply.github.com>
2023-12-10 16:47:44 +0100

    Changed environment variables

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,11 +1,7 @@
 export const environment = {
   production: false,
-  backendProducts: "/assets/mock/products.json",
-  backendLoginClient: "http://localhost:443/api/utilisateur/login",
-  backendCreateClient: "http://localhost:443/api/utilisateur",
-  backendCatalogue: "http://localhost:443/api/catalogue",
-  backendCatalogueSearch: "http://localhost:443/api/catalogue/search",
-  //   backendLoginClient: 'https://tp05-864a.onrender.com/api/utilisateur/login',
-  //   backendCreateClient: 'https://tp05-864a.onrender.com/api/utilisateur',
-  //   backendCatalogue: 'https://tp05-864a.onrender.com/api/catalogue',
+  backendLoginClient: "https://tp06.onrender.com/api/utilisateur/login",
+  backendCreateClient: "https://tp06.onrender.com/api/utilisateur",
+  backendCatalogue: "https://tp06.onrender.com/api/catalogue",
+  backendCatalogueSearch: "https://tp06.onrender.com/api/catalogue/search",
 };
